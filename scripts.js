1497a1582c5643c3a0e42b1d53434213381df24b
Bob Guo <bobguo1234@gmail.com>
2023-11-06 15:44:19 +1100

    skills page

--- scripts.js
+++ scripts.js
@@ -1,11 +1,29 @@
-const tabs = document.querySelectorAll('.tab');
-const contentPages = document.querySelectorAll('.content-page');
+// JavaScript to toggle the theme
+document.addEventListener('DOMContentLoaded', (event) => {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        // Hide all content pages
-        contentPages.forEach(page => page.style.display = 'none');
-        // Show the corresponding content page
-        contentPages[index].style.display = 'block';
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            themeToggleButton.textContent = 'Switch to Light Mode';
+        } else {
+            themeToggleButton.textContent = 'Switch to Dark Mode';
+        }
+    }
+
+    themeToggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        document.body.classList.toggle('light-mode');
+
+        let theme = 'light-mode';
+        if (document.body.classList.contains('dark-mode')) {
+            theme = 'dark-mode';
+            themeToggleButton.textContent = 'Switch to Light Mode';
+        } else {
+            themeToggleButton.textContent = 'Switch to Dark Mode';
+        }
+        
+        localStorage.setItem('theme', theme);
     });
 });
--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('hi')
+import os
+
+# Define the directory path relative to the current script
+directory_path = 'images/skill icons'
+
+# Check if the directory exists
+if os.path.exists(directory_path):
+    # List all files and directories in the specified directory
+    for filename in os.listdir(directory_path):
+        print(filename)
+else:
+    print(f"The directory {directory_path} does not exist.")
